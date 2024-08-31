94511b3c6a273df9b06d2c449358ed86026f7a78
Nekka Amina <123459087+nekka69a@users.noreply.github.com>
2024-08-31 23:55:35 +0200

    change loops

--- src/forecast.js
+++ src/forecast.js
@@ -1,6 +1,6 @@
 import { apikey } from "./utils.js";
 
-export async function apiCallForecast() {
+export async function getForecastData() {
   document.querySelector("form").addEventListener("submit", async (evt) => {
     evt.preventDefault();
     const ville = document.querySelector("#input-search").value;
@@ -11,33 +11,27 @@ export async function apiCallForecast() {
       const response = await fetch(url);
       const dataForecast = await response.json();
 
-      //   ============= DATE ===========
       for (let i = 0; i < dataForecast.list.length; i += 8) {
+        const index = i / 8 + 1;
+
+        ///   ============= DATE ===========
         const date = dataForecast.list[i].dt_txt.split(" ")[0];
-        document.querySelector(`.date${i / 8 + 1}`).innerHTML = ` ${date}`;
-      }
+        document.querySelector(`.date${index}`).innerHTML = ` ${date}`;
 
-      //   ============= ICON ===========
-      for (let i = 0; i < dataForecast.list.length; i += 8) {
-        document.querySelector(`.icon${i / 8 + 1}`).innerHTML =
+        //   ============= ICON ===========
+        document.querySelector(`.icon${index}`).innerHTML =
           `<img class="weather-icon" src="https://openweathermap.org/img/wn/${dataForecast.list[i].weather[0].icon}@2x.png" alt="" />`;
-      }
 
-      //   ========= TEMPERATURE ========
-      for (let i = 0; i < dataForecast.list.length; i += 8) {
-        document.querySelector(`.temp${i / 8 + 1}`).innerHTML =
+        //   ========= TEMPERATURE ========
+        document.querySelector(`.temp${index}`).innerHTML =
           ` ${dataForecast.list[i].main.temp.toFixed(1)}°C`;
-      }
 
-      //   ============ MIN-MAX ==========
-      for (let i = 0; i < dataForecast.list.length; i += 8) {
-        document.querySelector(`.min-max${i / 8 + 1}`).innerHTML =
+        //   ============ MIN-MAX ==========
+        document.querySelector(`.min-max${index}`).innerHTML =
           ` Min: ${dataForecast.list[i].main.temp_min.toFixed(1)}°C - Max: ${dataForecast.list[i].main.temp_max.toFixed(1)}°C`;
-      }
 
-      //   ============= HUMIDITY =========
-      for (let i = 0; i < dataForecast.list.length; i += 8) {
-        document.querySelector(`.humidity${i / 8 + 1}`).innerHTML =
+        //   ============= HUMIDITY =========
+        document.querySelector(`.humidity${index}`).innerHTML =
           ` ${dataForecast.list[i].main.humidity} %`;
       }
     } catch (err) {
