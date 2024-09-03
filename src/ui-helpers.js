449e933517c8adf40be373dbf8ba9386bf02ac58
Nekka Amina <123459087+nekka69a@users.noreply.github.com>
2024-09-03 16:19:49 +0200

    fix bug

---
+++ src/ui-helpers.js
@@ -0,0 +1,141 @@
+import {
+  setWeatherLoader,
+  setForecastLoader,
+  setPollutionAirLoader,
+} from "./ui.js";
+
+/** *
+ * Displays weather data
+ * @param {Object} weatherData -- The weather data object
+ */
+const displayWeatherDetails = (weatherDetails) => {
+  document.querySelector(".city").innerHTML = `${weatherDetails.name}`;
+  document.querySelector(".temp").innerHTML =
+    ` ${Math.round(weatherDetails.main.temp)}°C`;
+  document.querySelector(".feel").innerHTML =
+    `Ressenti: ${Math.round(weatherDetails.main.feels_like)}°C`;
+  document.querySelector(".icon").innerHTML =
+    `<img class="weather-icon" src="https://openweathermap.org/img/wn/${weatherDetails.weather[0].icon}@2x.png" alt=""/>`;
+  document.querySelector(".weather-city").innerHTML =
+    weatherDetails.weather[0].description;
+  document.querySelector(".humidity-icon").innerHTML =
+    '<img class="img-icon" src="../assets/humidite (1).png "alt="" />';
+  document.querySelector(".humidity-data").innerHTML =
+    `Humidité : ${weatherDetails.main.humidity} %`;
+  document.querySelector(".minmax-icon").innerHTML =
+    '<img class="img-icon" src="../assets/thermometre.png "alt="" />';
+  document.querySelector(".minmax-data").innerHTML = `Min: ${Math.round(
+    weatherDetails.main.temp_min,
+  )}°C</br>Max: ${Math.round(weatherDetails.main.temp_max)}°C`;
+  document.querySelector(".wind-icon").innerHTML =
+    '<img class="img-icon" src="../assets/vent (1).png "alt="" />';
+  document.querySelector(".wind-speed").innerHTML =
+    `${weatherDetails.wind.speed} m/s</br>Direction: ${weatherDetails.wind.deg}°`;
+  document.querySelector(".pressure-icon").innerHTML =
+    '<img class="img-icon" src="../assets/pression.png "alt="" />';
+  document.querySelector(".pressure-data").innerHTML =
+    `${weatherDetails.main.pressure} hPa`;
+  document.querySelector(".visibility-icon").innerHTML =
+    '<img class="img-icon" src="../assets/oeil.png "alt="" />';
+  document.querySelector(".visibility-data").innerHTML = `${
+    weatherDetails.visibility / 1000
+  } Km`;
+};
+
+/** *
+ * Displays forecast weather data
+ * @param {Object} forecastData -- The forecast weather data object
+ */
+
+const displayForecast = (forecastDetails) => {
+  for (let i = 0; i < forecastDetails.list.length; i += 8) {
+    const index = i / 8 + 1;
+
+    ///   ============= DATE ===========
+    const date = forecastDetails.list[i].dt_txt.split(" ")[0];
+    document.querySelector(`.date${index}`).innerHTML = ` ${date}`;
+
+    //   ============= ICON ===========
+    document.querySelector(`.icon${index}`).innerHTML =
+      `<img class="weather-icon" src="https://openweathermap.org/img/wn/${forecastDetails.list[i].weather[0].icon}@2x.png" alt="" />`;
+
+    //   ========= TEMPERATURE ========
+    document.querySelector(`.temp${index}`).innerHTML =
+      ` ${forecastDetails.list[i].main.temp.toFixed(1)}°C`;
+
+    //   ============ MIN-MAX ==========
+    document.querySelector(`.min-max${index}`).innerHTML =
+      ` Min: ${forecastDetails.list[i].main.temp_min.toFixed(1)}°C - Max: ${forecastDetails.list[i].main.temp_max.toFixed(1)}°C`;
+
+    //   ============= HUMIDITY =========
+    document.querySelector(`.humidity${index}`).innerHTML =
+      ` ${forecastDetails.list[i].main.humidity} %`;
+  }
+};
+
+/** *
+ * Displays air pollution data
+ * @param {Object} aiPollutionData -- The air pollution data object
+ */
+
+const displayAirPollution = (airPollutionDetails) => {
+  document.querySelector(".indice").innerHTML =
+    `Indice de qualité de l'air : ${airPollutionDetails.list[0].main.aqi}`;
+  document.querySelector(".co").innerHTML =
+    `CO : ${airPollutionDetails.list[0].components.co} µg/m³`;
+  document.querySelector(".no").innerHTML =
+    `NO : ${airPollutionDetails.list[0].components.no} µg/m³`;
+  document.querySelector(".no2").innerHTML =
+    `NO² : ${airPollutionDetails.list[0].components.no2} µg/m³`;
+  document.querySelector(".o3").innerHTML =
+    `O3 : ${airPollutionDetails.list[0].components.o3} µg/m³`;
+  document.querySelector(".so2").innerHTML =
+    `SO² : ${airPollutionDetails.list[0].components.so2} µg/m³`;
+  document.querySelector(".pm2").innerHTML =
+    `PM2.5 : ${airPollutionDetails.list[0].components.pm2_5} µg/m³`;
+  document.querySelector(".pm10").innerHTML =
+    `PM10 : ${airPollutionDetails.list[0].components.pm10} µg/m³`;
+  document.querySelector(".nh3").innerHTML =
+    `NH3 : ${airPollutionDetails.list[0].components.nh3} µg/m³`;
+};
+
+/**
+ * Display the UI width the fetched weather, forecast and air pollution data
+ * @param {Object} weatherData -- The weather data object
+ * @param {Object} forecastData -- The forecast data object
+ * @param {Object} airPollutionData -- The air pollution data object
+ * @returns {void}
+ */
+
+const displayWeather = (weatherData, forecastData, airPollutionData) => {
+  displayWeatherDetails(weatherData);
+  displayForecast(forecastData);
+  displayAirPollution(airPollutionData);
+};
+
+/**
+ * Updates the UI width the fetched weather, forecast and air pollution data
+ * @param {Object} weatherData -- The weather data object
+ * @param {Object} forecastData -- The forecast data object
+ * @param {Object} airPollutionData -- The air pollution data object
+ * @returns {void}
+ */
+
+const updateUI = (weatherData, forecastData, airPollutionData) => {
+  if (!weatherData || !forecastData || !airPollutionData) {
+    console.log("Il manque un jeu de données");
+    return;
+  }
+
+  console.log("Weather Data:", weatherData);
+  console.log("Forecast Data:", forecastData);
+  console.log("Air Pollution Data:", airPollutionData);
+
+  setWeatherLoader(false);
+  setForecastLoader(false);
+  setPollutionAirLoader(false);
+
+  displayWeather(weatherData, forecastData, airPollutionData);
+};
+
+export default updateUI;
--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,8 @@
-import { getUserGeoLocation } from "./api.js";
+import {
+  fetchAirPollutionByGeocode,
+  fetchWeatherForecastByGeocode,
+  getUserGeoLocation,
+} from "./api.js";
 
 /**
  * Set loader for weather Details
@@ -105,76 +109,23 @@ const fetchWeatherData = async (city) => {
   }
 
   const { lat, lon } = geoCode;
-  console.log(`lat: ${lat} | lon: ${lon}`);
 
-  //  appeler toutes les fonctions pour récupérer la data;
+  const weatherDataPromise = fetchWeatherData(lat, lon);
+  const weatherForecastDataPromise = fetchWeatherForecastByGeocode(lat, lon);
+  const airPollutionDataPromise = fetchAirPollutionByGeocode(lat, lon);
+
+  const [weatherData, forecastData, airPollutionData] = await Promise.all([
+    weatherDataPromise,
+    weatherForecastDataPromise,
+    airPollutionDataPromise,
+  ]);
+
+  updateUI(weatherData, forecastData, airPollutionData);
 };
-
-const updateCurrentWeatherData = (city, dataWeather) => {
-  document.querySelector(".city").innerHTML = `${city}`;
-  document.querySelector(".temp").innerHTML =
-    ` ${Math.round(dataWeather.main.temp)}°C`;
-  document.querySelector(".feel").innerHTML =
-    `Ressenti: ${Math.round(dataWeather.main.feels_like)}°C`;
-  document.querySelector(".icon").innerHTML =
-    `<img class="weather-icon" src="https://openweathermap.org/img/wn/${dataWeather.weather[0].icon}@2x.png" alt=""/>`;
-  document.querySelector(".weather-city").innerHTML =
-    dataWeather.weather[0].description;
-  document.querySelector(".humidity-icon").innerHTML =
-    '<img class="img-icon" src="../assets/humidite (1).png "alt="" />';
-  document.querySelector(".humidity-data").innerHTML =
-    `Humidité : ${dataWeather.main.humidity} %`;
-  document.querySelector(".minmax-icon").innerHTML =
-    '<img class="img-icon" src="../assets/thermometre.png "alt="" />';
-  document.querySelector(".minmax-data").innerHTML = `Min: ${Math.round(
-    dataWeather.main.temp_min,
-  )}°C</br>Max: ${Math.round(dataWeather.main.temp_max)}°C`;
-  document.querySelector(".wind-icon").innerHTML =
-    '<img class="img-icon" src="../assets/vent (1).png "alt="" />';
-  document.querySelector(".wind-speed").innerHTML =
-    `${dataWeather.wind.speed} m/s</br>Direction: ${dataWeather.wind.deg}°`;
-  document.querySelector(".pressure-icon").innerHTML =
-    '<img class="img-icon" src="../assets/pression.png "alt="" />';
-  document.querySelector(".pressure-data").innerHTML =
-    `${dataWeather.main.pressure} hPa`;
-  document.querySelector(".visibility-icon").innerHTML =
-    '<img class="img-icon" src="../assets/oeil.png "alt="" />';
-  document.querySelector(".visibility-data").innerHTML = `${
-    dataWeather.visibility / 1000
-  } Km`;
-};
-
-const updateAirQualityData = (dataAir) => {
-  document.querySelector(".indice").innerHTML =
-    `Indice de qualité de l'air : ${dataAir.list[0].main.aqi}`;
-  document.querySelector(".co").innerHTML =
-    `CO : ${dataAir.list[0].components.co} µg/m³`;
-  document.querySelector(".no").innerHTML =
-    `NO : ${dataAir.list[0].components.no} µg/m³`;
-  document.querySelector(".no2").innerHTML =
-    `NO² : ${dataAir.list[0].components.no2} µg/m³`;
-  document.querySelector(".o3").innerHTML =
-    `O3 : ${dataAir.list[0].components.o3} µg/m³`;
-  document.querySelector(".so2").innerHTML =
-    `SO² : ${dataAir.list[0].components.so2} µg/m³`;
-  document.querySelector(".pm2").innerHTML =
-    `PM2.5 : ${dataAir.list[0].components.pm2_5} µg/m³`;
-  document.querySelector(".pm10").innerHTML =
-    `PM10 : ${dataAir.list[0].components.pm10} µg/m³`;
-  document.querySelector(".nh3").innerHTML =
-    `NH3 : ${dataAir.list[0].components.nh3} µg/m³`;
-};
-
-function clearCity() {
-  const input = document.querySelector("#input-search");
-  input.addEventListener("click", () => {
-    document.querySelector(".city").innerHTML = "";
-  });
-}
 
 export {
   setupEventListeners,
-  updateCurrentWeatherData,
-  updateAirQualityData,
-  clearCity,
+  setWeatherLoader,
+  setForecastLoader,
+  setPollutionAirLoader,
 };
